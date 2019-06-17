e239e1e1ed63116f4200d5dbffe2c5ba19088b2a
zhuo <hczyzlm@126.com>
2019-06-17 21:54:58 +0800

    feat: 修改脚本

--- install.sh
+++ install.sh
@@ -1,29 +1,36 @@
 #!/usr/make
 
+# 代码模板目录
+# Work/youzan/XcodeTmplates
+# CodeSnip
+# ~/Library/Developer/Xcode/UserData/CodeSnippets/
+# 运行命令
+# curl https://github.com/hcxyzlm/XcodeTmplates/blob/master/install.sh -s | sh
+
 # prepare source
-codesnippets_zip=codesnippets.zip
-xctemplates_zip=xctemplates.zip
+codesnippets_dic=xctemplates
+xctemplates_dic=codesnippets
 
-local_codesnippets_zip=`pwd`/archive/$codesnippets_zip
-local_xctemplates_zip=`pwd`/archive/$xctemplates_zip
+local_codesnippets_dic=`pwd`/$codesnippets_dic
+local_xctemplates_dic=`pwd`/$xctemplates_dic
 
-remote_url="https://github.com/hcxyzlm/XcodeTmplates/archive"
+remote_url="https://github.com/hcxyzlm/XcodeTmplates"
 temp_directory=`mktemp -d`
 
-if [ ! -f "$local_codesnippets_zip" ]; then
+if [ ! -f "$local_codesnippets_dic" ]; then
     # fetch remote codesnippets zip    
-    remote_codesnippets_zip=$remote_url/$codesnippets_zip
-    local_codesnippets_zip=$temp_directory/$codesnippets_zip
-    curl $remote_codesnippets_zip -o $local_codesnippets_zip -s
-    echo "fetch $remote_codesnippets_zip to $local_codesnippets_zip"
+    remote_codesnippets_dic=$remote_url/$codesnippets_dic
+    local_codesnippets_dic=$temp_directory/$xctemplates_dic
+    curl $remote_codesnippets_dic -o $local_codesnippets_dic -s
+    echo "fetch $remote_codesnippets_dic to $local_codesnippets_dic"
 fi
 
-if [ ! -f "$local_xctemplates_zip" ]; then
+if [ ! -f "$local_xctemplates_dic" ]; then
     # fetch remote xctemplates zip    
-    remote_xctemplates_zip=$remote_url/$xctemplates_zip
-    local_xctemplates_zip=$temp_directory/$xctemplates_zip
-    curl $remote_xctemplates_zip -o $local_xctemplates_zip -s
-    echo "fetch $remote_xctemplates_zip to $local_xctemplates_zip"    
+    remote_xctemplates_dic=$remote_url/$xctemplates_dic
+    local_xctemplates_dic=$temp_directory/$xctemplates_dic
+    curl $remote_xctemplates_dic -o $local_xctemplates_dic -s
+    echo "fetch $remote_xctemplates_dic to $local_xctemplates_dic"    
 fi
 
 templates=~/Library/Developer/Xcode/Templates
@@ -44,16 +51,16 @@ rm -f "$target_codesnippets"/WCDB.*
 rm -fr "$target_templates"/WCDB
 
 install() {
-    source_zip="$1"
+    source_dic="$1"
     target="$2"
     mkdir -p "$target"
-    unzip -o -q "$source_zip" -d "$target"
+    cp -r "$source_dic" "$target"
 }
 
 # install code templates
-install "$local_xctemplates_zip" "$target_templates"
+install "$local_xctemplates_dic" "$target_templates"
 echo "Code templates is installed at $target_templates"
 
 # install codesnippets
-install "$local_codesnippets_zip" "$target_codesnippets"
+install "$local_codesnippets_dic" "$target_codesnippets"
 echo "Code snippets is installed at $target_codesnippets"
