4f0f9e05ea64085109c65fae107cb36d8bac1e74
zhuo <hczyzlm@126.com>
2019-06-18 10:21:21 +0800

    feat: 添加注释

--- install.sh
+++ install.sh
@@ -17,28 +17,30 @@ local_xctemplates_zip=`pwd`/archive/$xctemplates_zip
 remote_url="https://raw.githubusercontent.com/hcxyzlm/XcodeTmplates/master/archive"
 temp_directory=`mktemp -d`
 
+# 用curl 下载codesnippets.zip
 if [ ! -f "$local_codesnippets_zip" ]; then
-    # fetch remote codesnippets zip    
     remote_codesnippets_zip=$remote_url/$codesnippets_zip
     local_codesnippets_zip=$temp_directory/$codesnippets_zip
     curl $remote_codesnippets_zip -o $local_codesnippets_zip -s
     echo "fetch $remote_codesnippets_zip to $local_codesnippets_zip"
 fi
 
-if [ ! -f "$local_xctemplates_zip" ]; then
-    # fetch remote xctemplates zip    
+# # 用curl xctemplates.zip
+if [ ! -f "$local_xctemplates_zip" ]; then  
     remote_xctemplates_zip=$remote_url/$xctemplates_zip
     local_xctemplates_zip=$temp_directory/$xctemplates_zip
     curl $remote_xctemplates_zip -o $local_xctemplates_zip -s
     echo "fetch $remote_xctemplates_zip to $local_xctemplates_zip"    
 fi
 
-templates=~/Library/Developer/Xcode/Templates
 
+templates=~/Library/Developer/Xcode/Templates
+# xcode 模板工程路径
 target_templates="$templates"/File\ Templates/
+# # xcode codesnipets 路径
 target_codesnippets=~/Library/Developer/Xcode/UserData/CodeSnippets/
 
-# handle unexpected issue
+# 删除之前的资源
 if [ -f "$templates" ] ;
 then
     rm "$templates"
@@ -50,6 +52,7 @@ fi;
 rm -f "$target_codesnippets"/yz_*
 rm -fr "$target_templates"/YouZan
 
+# 用函数来安装文件
 install() {
     source_zip="$1"
     target="$2"
@@ -57,10 +60,10 @@ install() {
     unzip -o -q "$source_zip" -d "$target"
 }
 
-# install code templates
+# 安装 xctemplates
 install "$local_xctemplates_zip" "$target_templates"
 echo "Code templates is installed at $target_templates"
 
-# install codesnippets
+# 安装 codesnippets
 install "$local_codesnippets_zip" "$target_codesnippets"
 echo "Code snippets is installed at $target_codesnippets"
